35a279f71876172b8944d0948048a0a82db363ad
zanzibra <talbergaerika@gmail.com>
2022-10-25 21:11:17 +0300

    1st try

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if (true) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
